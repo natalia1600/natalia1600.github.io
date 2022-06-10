5dd7f6bb772aa680722b2cc0fc12ccaa6ea26c07
Amy Huang <amyxjhuang@berkeley.edu>
2022-06-10 15:52:32 -0700

    styling

--- web/src/App.js
+++ web/src/App.js
@@ -11,16 +11,20 @@ function App() {
         <div className="row">
            {/* picture of self */}
             <div>
-              <img src={require("./assets/me.png")} />
+              <img src={require("./assets/group1.png")} />
             </div>
             <div id="blurb">
               <p>
                 hello! i'm natalia, a senior at uc berkeley studying data science and economics.
                 <br/><br/> i like python, big data, design, and the beatles. check out some of my technical projects here or reach me at natalia16@gmail.com!
               </p>
+
+              <a href=""><p className="button">resume</p></a>
+              <a href=""><p className="button">github</p></a>
+              <a href=""><p className="button">linkedin</p></a>
               {/* cartoon aesthetic pic */}
-              <img src={require("./assets/cat.png")}/>
-              <img src={require("./assets/working.png")}/>
+              {/* <img src={require("./assets/cat.png")}/>
+              <img src={require("./assets/working.png")}/> */}
             </div>
         </div>
         
