9dd8fc2c343cfd5c6998174fc8d9a3e3a6605390
Amy Huang <amyxjhuang@berkeley.edu>
2022-06-10 18:42:22 -0700

    responsive web design

--- web/src/App.js
+++ web/src/App.js
@@ -15,13 +15,16 @@ function App() {
             </div>
             <div id="blurb">
               <p>
-                hello! i'm natalia, a senior at uc berkeley studying data science and economics.
+                hello n welcome to my portfolio! i'm natalia, a senior at uc berkeley studying data science and economics.
                 <br/><br/> i like python, big data, design, and the beatles. check out some of my technical projects here or reach me at natalia16@gmail.com!
+
+                <br/><br/> this website was designed in figma and implemented w/ react js
+              
               </p>
 
-              <a href=""><p className="button">resume</p></a>
-              <a href=""><p className="button">github</p></a>
-              <a href=""><p className="button">linkedin</p></a>
+              <a target="_blank" href=""><p className="button">resume</p></a>
+              <a target="_blank" href="https://github.com/natalia1600"><p className="button">github</p></a>
+              <a target="_blank" href="https://linkedin.com/in/natalia16"><p className="button">linkedin</p></a>
               {/* cartoon aesthetic pic */}
               {/* <img src={require("./assets/cat.png")}/>
               <img src={require("./assets/working.png")}/> */}
@@ -31,8 +34,52 @@ function App() {
       </header>
 
       <body className="App-body">
-        <h3>projects</h3>
-        <p>alliance for inclusive AI website: </p>
+        <h2>projects</h2>
+        {/* aiai website proj */}
+          <div className="row">
+          <div>
+          <a target="_blank" href="https://aiai.berkeley.edu">
+            <p className="button">
+            🍥 Alliance for Inclusive AI Website</p> 
+            </a>
+            <p>I designed and maintained this website for Berkeley HAAS Alliance for Inclusive AI,
+              which is visited by hundreds of people every month! This process included prototyping 
+              on Adobe InDesign, deploying via Wordpress, and studying web design principles. </p>
+          </div>
+          <img className="proj-pic" src={require("./assets/aiai.png")} />
+
+          </div>
+
+        {/* empatica */}
+        <div className="row">
+          <div>
+          <a target="_blank" href="//https://connectedlearning.uci.edu/person/stephen-schueller/">
+            <p className="button">
+            🩺 iOS App: Wearable Medical Device</p> 
+            </a>
+            <p>My first time doing iOS development using Swift! I used the <a href="https://developer.empatica.com/">Empatica API</a> to
+             establish a bluetooth connection, and created a questionnaire component of the app. I learned about modal components, 
+            Core Data, and incorporating API into projects.</p>
+          </div>
+          <img className="proj-pic" src={require("./assets/aiai.png")} />
+
+          </div>
+
+        {/* game */}
+        <div className="row">
+          <div>
+          <a target="_blank" href="//https://connectedlearning.uci.edu/person/stephen-schueller/">
+            <p className="button">
+            👾2D World Generator</p> 
+            </a>
+            <p>I used Java to implement this psuedo-random world generator for game design. To do this, 
+              I used __ data structures and ___ algorithms. The game is fully playable and I also designed the visuals from scratch! 
+
+            </p>
+          </div>
+          <img className="proj-pic" src={require("./assets/aiai.png")} />
+
+          </div>
 
       </body>
     </div>
